b3a7bebe6e9c26fb5779e787dd0e1a11158d53e5
ikuya126 <ikuya126@gmail.com>
2025-03-25 15:39:21 +0900

    新規プロジェクト作成

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use App\Http\Requests\ContactRequest;
+use App\Mail\ContactAdminMail;
+use Illuminate\Support\Facades\Mail;
+
+class ContactController extends Controller
+{
+    public function index()
+    {
+        return view('contact.index');
+    }
+
+    public function complete()
+    {
+        return view('contact.complete');
+    }
+
+    public function sendMail(ContactRequest $request) {
+    $validated = $request->validated();
+
+    Mail::to('admin@example.com')->send(new ContactAdminMail($validated));
+    return to_route('contact.complete');
+}
+}
---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
+
+Route::view('/', 'index');
+
+Route::get('/contact', [ContactController::class,'index'])->name('contact');
+Route::post('/contact',[ContactController::class,'sendMail'])->name('contact.sendMail');
+Route::get('/contact/complete', [ContactController::class,'complete'])->name('contact.complete');
